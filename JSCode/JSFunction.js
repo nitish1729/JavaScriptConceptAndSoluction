e64ae27d0566db2a06eef6c7f71c3059d9632a59
Nitish Mehta <nkrmehta09@gmail.com>
2022-08-06 02:45:55 +0530

    practice section

---
+++ JSCode/JSFunction.js
@@ -0,0 +1,17 @@
+      //  JavaScript Function Definitions
+      // functions are defined with the function keyword.
+      console.log("JS functions are defined with the function keyword.");
+                  //Function Declarations
+    console.log("JS Function Declarations");
+function addition(x,y)
+{
+    console.log("Done");
+    return (x + y)/2;
+}
+let a = 10;
+let b =20;
+let c = 40;
+let x,y;
+console.log(" Addtion of a and b is ", addition(a,b));
+console.log(" Addtion of b and c is ", addition(b,c));
+console.log(" Addtion of c and a is ", addition(c,a));
--- UserInput.js
+++ UserInput.js
@@ -1,6 +1,17 @@
-" use strict";
+// " use strict";
 
-const ps = require("prompt-sync");
-const prompt = ps();
-let name = prompt("enter your name :")
-console.log('Hello ${name}');
+// const ps = require("prompt-sync");
+// const prompt = ps();
+// let name = prompt("enter your name :")
+// console.log('Hello ${name}');
+// work in browser
+let a1 = prompt("Hey Dude, What's ur name")
+let a = prompt("Hey Dude, What's ur age");
+a = Number.parseInt(a);
+if (a >18)
+{
+    console.log("you can ");
+}
+else{
+    console.log("you cann't ");
+}
